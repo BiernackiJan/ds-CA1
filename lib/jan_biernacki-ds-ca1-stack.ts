10cf2c6277234a9bbd526a717ea1e91638509fc7
Jan <janbier2000@gmail.com>
2024-11-09 18:41:07 +0000

    Implemented signup and verification of users

--- lib/jan_biernacki-ds-ca1-stack.ts
+++ lib/jan_biernacki-ds-ca1-stack.ts
@@ -20,94 +20,12 @@ export class JanBiernackiDsCa1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
-  //   //authenticatioon
-  //   const userPool = new UserPool(this, "UserPool", {
-  //     signInAliases: { username: true, email: true },
-  //     selfSignUpEnabled: true,
-  //     removalPolicy: cdk.RemovalPolicy.DESTROY,
-  //   });
-
-  //   this.userPoolId = userPool.userPoolId;
-
-  //   const appClient = userPool.addClient("AppClient", {
-  //     authFlows: { userPassword: true },
-  //   });
-
-  //   this.userPoolClientId = appClient.userPoolClientId;
-
-  //   const authApi = new apig.RestApi(this, "AuthServiceApi", {
-  //     description: "Authentication Service RestApi",
-  //     endpointTypes: [apig.EndpointType.REGIONAL],
-  //     defaultCorsPreflightOptions: {
-  //       allowOrigins: apig.Cors.ALL_ORIGINS,
-  //     },
-  //   });
-
-
-  //   this.auth = authApi.root.addResource("auth");
-
-
-  //   this.addAuthRoute(
-  //     "signup",
-  //     "POST",
-  //     "SignupFn",
-  //     'signup.ts'
-  //   );
-
-  //   this.addAuthRoute(
-  //     "confirm_signup",
-  //     "POST",
-  //     "ConfirmFn",
-  //     "confirm-signup.ts"
-  //   );
-
-  // }
-
-  
-
-  // private addAuthRoute(
-  //   resourceName: string,
-  //   method: string,
-  //   fnName: string,
-  //   fnEntry: string,
-  //   allowCognitoAccess?: boolean
-  // ): void {
-  //   const commonFnProps = {
-  //     architecture: lambda.Architecture.ARM_64,
-  //     timeout: cdk.Duration.seconds(10),
-  //     memorySize: 128,
-  //     runtime: lambda.Runtime.NODEJS_18_X,
-  //     handler: "handler",
-  //     environment: {
-  //       USER_POOL_ID: this.userPoolId,
-  //       CLIENT_ID: this.userPoolClientId,
-  //       REGION: cdk.Aws.REGION
-  //     },
-  //   };
-    
-  //   const resource = this.auth.addResource(resourceName);
-    
-  //   const fn = new node.NodejsFunction(this, fnName, {
-  //     ...commonFnProps,
-  //     entry: `${__dirname}/../lambdas/auth/${fnEntry}`,
-  //   });
-
-  //   resource.addMethod(method, new apig.LambdaIntegration(fn));
-
     const ca1Fn = new lambdanode.NodejsFunction(this, "Ca1Fn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_18_X,
       entry: `${__dirname}/../lambdas/ca1.ts`,
       timeout: cdk.Duration.seconds(10),
       memorySize: 128,
-    });
-
-    const ca1FnURL = ca1Fn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.AWS_IAM,
-      cors: {
-        allowedOrigins: ["*"],
-      },
     });
 
 
@@ -282,7 +200,72 @@ export class JanBiernackiDsCa1Stack extends cdk.Stack {
       "DELETE",
       new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
     );
+
+
+    //authenticatioon
+    const userPool = new UserPool(this, "UserPool", {
+  
+    signInAliases: { username: true, email: true },
+    selfSignUpEnabled: true,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.userPoolId = userPool.userPoolId;
+
+    const appClient = userPool.addClient("AppClient", {
+      authFlows: { userPassword: true },
+    });
+
+    this.userPoolClientId = appClient.userPoolClientId;
+    this.auth = api.root.addResource("auth");
+
+    this.addAuthRoute(
+      "signup",
+      "POST",
+      "SignupFn",
+      'signup.ts'
+    );
+
+    this.addAuthRoute(
+      "confirm_signup",
+      "POST",
+      "ConfirmFn",
+      "confirm-signup.ts"
+    );
+
     
     
+  }
+
+  
+
+private addAuthRoute(
+  resourceName: string,
+  method: string,
+  fnName: string,
+  fnEntry: string,
+  allowCognitoAccess?: boolean
+): void {
+  const commonFnProps = {
+    architecture: lambda.Architecture.ARM_64,
+    timeout: cdk.Duration.seconds(10),
+    memorySize: 128,
+    runtime: lambda.Runtime.NODEJS_18_X,
+    handler: "handler",
+    environment: {
+    USER_POOL_ID: this.userPoolId,
+      CLIENT_ID: this.userPoolClientId,
+      REGION: cdk.Aws.REGION
+    },
+  };
+    
+  const resource = this.auth.addResource(resourceName);
+    
+  const fn = new node.NodejsFunction(this, fnName, {
+    ...commonFnProps,
+    entry: `${__dirname}/../lambdas/auth/${fnEntry}`,
+  });
+
+  resource.addMethod(method, new apig.LambdaIntegration(fn));
   }
 }
